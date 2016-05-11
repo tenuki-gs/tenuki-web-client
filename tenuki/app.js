a360db5790b3879460f402be351d6025a11a95fb
Kieran Sobel <kieran@tenuki.cafe>
2016-05-11 19:22:18 -0400

    Use babel for ES2015 and JSX

--- tenuki/app.js
+++ tenuki/app.js
@@ -1,3 +1,5 @@
-console.log("play somewhere else");
-
 require("../stylesheets/styles.scss");
+
+import React from 'react';
+
+console.log("play somewhere else");
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,23 @@
 module.exports = {
     entry: './tenuki/app.js',
+    resolve: {
+        extensions: ['', '.js']
+    },
     output: {
         publicPath: '/',
         path: 'build/',
         filename: 'bundle.js'
     },
     module: {
-      loaders: [
-        {
-          test: /\.scss$/,
-          loaders: ['style', 'css', 'sass']
-        }
-      ]
+        loaders: [
+            {
+                test: /\.scss$/,
+                loaders: ['style', 'css', 'sass']
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel',
+            }
+        ]
     }
 };
