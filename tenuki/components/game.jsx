bc44934e0ea11a266ac8a0e7a885f58c91665167
Kieran Sobel <kieran.sobel@newsela.com>
2016-10-16 18:04:43 -0400

    Abstracted GoGame model and pulled move logic into it

--- tenuki/components/game.jsx
+++ tenuki/components/game.jsx
@@ -3,13 +3,30 @@ import Position from './position';
 import Moves from './moves';
 import Board from './board';
 
-export default class Game extends Component{
+export default class Game extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            board: null
+        };
+    }
+
+    componentDidMount() {
+        // Listen for changes to the board.
+        this.props.game.onNewBoard(board => this.setState({board}));
+    }
+
     render() {
+        if (!this.state.board) {
+            // The board might be empty until the game initializes.
+            return null;
+        }
+
         return (
             <div className="game">
                 <Board
-                    game={this.props.game}
-                    onMove={this.props.onMove}
+                    board={this.state.board}
+                    onMove={this.props.game.addMove}
                 />
                 <div className="moves">
                     Game ID: {this.props.game.id}
--- tenuki/components/surface.jsx
+++ tenuki/components/surface.jsx
@@ -6,17 +6,48 @@ export default class Surface extends Component{
         this.onClick = this.onClick.bind(this);
     }
 
-    onClick (x, y) {
+    onClick(x, y) {
         this.props.onMove({x, y});
     }
 
     render() {
-        var game = this.props.game;
+        const width = this.props.board.width;
+        const height = this.props.board.height;
         const lines = [];
         const spaces = [];
+        const moves = [];
 
-        for (var y = 1; y <= game.rules.board.height; ++y) {
-            for (var x = 1; x <= game.rules.board.width; ++x) {
+        for (var y = 1.5; y <= height + 0.5; ++y) {
+            lines.push(
+                <line
+                    x1="1.5"
+                    y1={y}
+                    x2={height + 0.5}
+                    y2={y}
+                    stroke="black"
+                    strokeWidth="0.05"
+                />
+            )
+        }
+
+        for (var x = 1.5; x <= width + 0.5; ++x) {
+            lines.push(
+                <line
+                    x1={x}
+                    y1="1.5"
+                    x2={x}
+                    y2={width + 0.5}
+                    stroke="black"
+                    strokeWidth="0.1"
+                />
+            )
+        }
+
+        for (var y = 1; y <= height; ++y) {
+            for (var x = 1; x <= width; ++x) {
+                const position = this.props.board[x][y];
+
+                // Every position has an empty rectangle for detecting hover.
                 spaces.push(
                     <rect
                         x={x}
@@ -27,64 +58,16 @@ export default class Surface extends Component{
                         className="space"
                     />
                 )
-            }
-        }
 
-        for (var y = 1.5; y <= game.rules.board.height + 0.5; ++y) {
-            lines.push(
-                <line
-                    x1="1.5"
-                    y1={y}
-                    x2={game.rules.board.height + 0.5}
-                    y2={y}
-                    stroke="black"
-                    strokeWidth="0.05"
-                />
-            )
-        }
-
-        for (var x = 1.5; x <= game.rules.board.width + 0.5; ++x) {
-            lines.push(
-                <line
-                    x1={x}
-                    y1="1.5"
-                    x2={x}
-                    y2={game.rules.board.width + 0.5}
-                    stroke="black"
-                    strokeWidth="0.1"
-                />
-            )
-        }
-
-
-        const positions = this.props.positions;
-        var moves = [];
-
-        for (var i = 0; i < positions.length; ++i) {
-            if (positions[i].move == 'w') {
-                moves.push(
-                    <text
-                        x={positions[i].x}
-                        y={positions[i].y}
-                        fontSize="1"
-                    >⚪</text>
-                )
-            } else if (positions[i].move == 'b') {
-                moves.push(
-                    <text
-                        x={positions[i].x}
-                        y={positions[i].y}
-                        fontSize="1"
-                    >⚫</text>
-                )
-            } else {
-                moves.push(
-                    <text
-                        x={positions[i].x}
-                        y={positions[i].y}
-                        fontSize="1"
-                    > </text>
-                )
+                if (position.move) {
+                    moves.push(
+                        <text
+                            x={x}
+                            y={y}
+                            fontSize="1"
+                        >{position.move.stone}</text>
+                    )
+                }
             }
         }
 
